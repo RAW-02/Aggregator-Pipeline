77343b3a9f4dc7d9d620fa5aae074dc5daa464eb
Atharvalearner <atharvabanchhode09@gmail.com>
2026-06-22 18:53:18 +0530

    EPSS Dataset Scheduler

--- collectors/epss_dataset.py
+++ collectors/epss_dataset.py
@@ -24,7 +24,10 @@ class EPSSDataset:
 
     def load(self):
         scores = {}
+
         with gzip.open(self.FILE, "rt", encoding="utf-8") as f:
+            next(f)
+            
             reader = csv.DictReader(f)
 
             for row in reader:
