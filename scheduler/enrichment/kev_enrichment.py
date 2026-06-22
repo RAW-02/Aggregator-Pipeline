edc9054f55ce4f0857e44fc6db39c7224fb1212e
Atharvalearner <atharvabanchhode09@gmail.com>
2026-06-22 19:31:34 +0530

    KEV Dataset Scheduler

--- scheduler/enrichment/kev_enrichment.py
+++ scheduler/enrichment/kev_enrichment.py
@@ -19,3 +19,6 @@ class KEVEnrichmentJob(BaseEnrichmentJob):
             "cve_id": record["cve_id"],
             "fields": {"kev_status": status, "kev_processed": True},
         }
+    
+    def fetch_by_id(self, cve_id):
+        return self.is_known_exploited(cve_id)
